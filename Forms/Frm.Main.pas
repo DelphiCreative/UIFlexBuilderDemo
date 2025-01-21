7694043c80442d4dd77c3cf24988485e961356cd
DelphiCreative <diegohenriquecataneo@gmail.com>
2025-01-21 08:14:11 -0300

    Update Frm.Main

--- Forms/Frm.Main.pas
+++ Forms/Frm.Main.pas
@@ -4,10 +4,20 @@ interface
 
 uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
-  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs;
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs,
+  System.ImageList, FMX.ImgList, FMX.Layouts, FMX.Effects, FMX.Objects;
 
 type
   TfrmMain = class(TForm)
+    StyleBook1: TStyleBook;
+    Rectangle2: TRectangle;
+    ShadowEffect2: TShadowEffect;
+    vsbUIMenu: TVertScrollBox;
+    Rectangle1: TRectangle;
+    ShadowEffect3: TShadowEffect;
+    vsbUIList: TVertScrollBox;
+    flwHeader: TFlowLayout;
+    ImageList1: TImageList;
   private
     { Private declarations }
   public
